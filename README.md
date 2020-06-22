29dff33434d011eb0bd3bb2488e207acf74a6c9c
Yanagida Ayaka <rainymoment0616@gmail.com>
2020-06-22 15:53:17 +0900

    aboutページと音声・動画系

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# repository docs_a11y
+# docs_a11y
+Webアクセシビリティについてまとめてみた（Vuepress + Netlify）  
+https://docs-a11y.netlify.app
+
+## install
+```
+npm ci
+```
+
+## usage
+```
+npm run dev
+```
+
+## build
+```
+npm run build
+```
--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -6,5 +6,53 @@ module.exports = {
     '/': {
       lang: 'ja'
     }
+  },
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Webアクセシビリティとは？', link: '/about/' },
+      { text: 'チートシート', link: '/cheatsheet/' },
+    ],
+    search: false,
+    sidebar: [
+      {
+        title: 'Home',
+        path: '/'
+      },
+      {
+        title: 'Webアクセシビリティとは？',
+        path: '/about/'
+      },
+      {
+        title: 'チートシート',
+        path: '/cheatsheet/',
+        collapsable: false,
+        sidebarDepth: 1,
+        children: [
+          { title: 'トップ', path: '/cheatsheet/' },
+          { title: 'サイト全体・ページ全体', path: '/cheatsheet/all.html' },
+          { title: '動画・音声', path: '/cheatsheet/media.html' },
+          // { title: 'テキスト', path: '/cheatsheet/text.html' },
+          // { title: '制限時間つきコンテンツ', path: '/cheatsheet/limit.html' },
+          // { title: '画像', path: '/cheatsheet/figure.html' },
+          // { title: '感覚的特徴', path: '/cheatsheet/features.html' },
+          // { title: 'フォーム', path: '/cheatsheet/form.html' }
+        ]
+      },
+
+    ]
   }
+  // plugins: [
+  //   // you can use it multiple times
+  //   // so babel-style may be a better choice
+  //   ['container', {
+  //     type: 'right',
+  //     defaultTitle: '',
+  //   }],
+  //   ['container', {
+  //     type: 'theorem',
+  //     before: info => `<div class="theorem"><p class="title">${info}</p>`,
+  //     after: '</div>',
+  //   }],
+  // ]
 }
